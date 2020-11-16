ddc7f34a8b4306f347a1d69f3b0ff0e52fca3524
juacq97 <juacq97@tutanota.com>
2020-11-15 20:37:52 -0700

    Añadido winview

--- config.def.h
+++ config.def.h
@@ -125,6 +125,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_o,      winview,        {0} },
 };
 
 /* button definitions */
